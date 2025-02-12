be002c9b126b27ef48670ad2d3659c352a0b04bd
陳德生 <develop@doeshing.ink>
2025-02-12 17:27:12 +0800

    fix: default language fallback to Traditional Chinese (zh-TW)

--- apps/theme-cosy/src/extend/blog_language.js
+++ apps/theme-cosy/src/extend/blog_language.js
@@ -4,5 +4,5 @@ hexo.extend.helper.register("blog_language", function () {
     'zh-CN': '简体中文',
     'en': 'English'
   }
-  return mapping[this.config['language'] ?? 'zh-CN']
+  return mapping[this.config['language'] ?? 'zh-TW']
 })
